60709fd8ce6311a3572ebf7c472198bdb04e921e
leon zhao <lionzjg00@hotmail.com>
2023-06-21 00:00:55 +0200

    4.0

---
+++ index.js
@@ -0,0 +1,51 @@
+
+        const colors = ['red', 'blue', 'green', 'pink', 'orange'];
+        let index = 0;
+
+        function changeColorById(id) {
+            let ele = document.getElementById(id);
+            index += 1;
+            index %= colors.length;
+            ele.style.color = colors[index];
+        }
+        function changeColor() {
+            let eles = document.getElementsByClassName("list_item");
+
+            index += 1;
+            index %= colors.length;
+
+            for (let i = 0; i < eles.length; i++) {
+                eles[i].style.color = colors[index];
+            }
+        }
+
+        function calcInput() {
+            let ele = document.getElementById("abc");
+            let numbers = ele.value.split(',');
+            let sum = calc(numbers);
+
+            let dis = document.getElementById("display");
+            dis.innerText = sum;
+
+        }
+
+        function calcMsg() {
+            let str = prompt("Please input numbers:");
+            let numbers = str.split(',');
+            let sum = calc(numbers);
+
+            let dis = document.getElementById("display");
+            dis.innerText = sum;
+        }
+
+        const add = (a, b) => { return a + b; }
+
+        function calc(numbers) {
+            let sum = 0;
+
+            for (let i = 0; i < numbers.length; i++) {
+                sum = add(sum, parseInt(numbers[i]));
+            }
+
+            return sum;
+        }
